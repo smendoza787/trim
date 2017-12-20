ee01e54c477c9d83f17c9571f5df1251afa95c8b
Sergio Mendoza <smendoza787@gmail.com>
2017-12-19 19:59:35 -0800

    add footer component

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.css';
+
+const Footer = () =>
+  <div className="footer">
+    <div className="contact">
+        <a href="#">+14248293749</a>
+        <a href="#">thisisatest@gmail.com</a>
+    </div>
+    <div className="copyright">
+      &copy; The Testing Company. All Rights Reserved.
+    </div>
+  </div>
+
+export default Footer;
