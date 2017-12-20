bc896f466dcfef1bc2460128af0aa114c340ba80
Sergio Mendoza <smendoza787@gmail.com>
2017-12-19 19:59:14 -0800

    add url shortener box

---
+++ client/src/components/UrlShortener/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class UrlShortener extends Component {
+  render() {
+    return (
+      <div className="url-shortener">
+        
+      </div>
+    )
+  }
+}
+
+export default UrlShortener;
