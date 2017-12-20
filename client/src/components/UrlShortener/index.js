c480c31df1738f614c4696e825f3defe76d0ea60
Sergio Mendoza <smendoza787@gmail.com>
2017-12-19 21:03:32 -0800

    insert state into UrlShortener parent component

--- client/src/components/UrlShortener/index.js
+++ client/src/components/UrlShortener/index.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import UrlInput from '../UrlInput';
 import './index.css';
 
 class UrlShortener extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      value: ''
+    }
+  }
+
+  onChange (input) {
+    // parent class change handler is always called with field name and value
+    this.setState({ value: input });
+  }
+  
   render() {
     return (
       <div className="url-shortener">
         <h1>Shorten any URL in less than a second.</h1>
         <h2>Make it easier to send and embed links.</h2>
+        <UrlInput onChange={this.onChange.bind(this)} longUrl={this.state.value} />
       </div>
     )
   }
