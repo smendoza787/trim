f80f0b86af35597e5429c52ec8e3965d35dea8f9
Sergio Mendoza <smendoza787@gmail.com>
2017-12-19 20:19:07 -0800

    add url shortener text

--- client/src/components/UrlShortener/index.js
+++ client/src/components/UrlShortener/index.js
@@ -5,7 +5,8 @@ class UrlShortener extends Component {
   render() {
     return (
       <div className="url-shortener">
-        
+        <h1>Shorten any URL in less than a second.</h1>
+        <h2>Make it easier to send and embed links.</h2>
       </div>
     )
   }
