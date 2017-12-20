dd9c50b5eab00311adcda363c279797569150356
Sergio Mendoza <smendoza787@gmail.com>
2017-12-19 23:55:37 -0800

    add handleFocus method to UrlOutput component to highlight text

--- client/src/components/UrlOutput/index.js
+++ client/src/components/UrlOutput/index.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import './index.css';
 
 class UrlOutput extends Component {
+  handleFocus(event) {
+    event.target.select();
+  }
+  
   render() {
     return (
       <div className="url-output">
         <h3>Shortened URL</h3>
-        <input className="url-output-field" type="text" value={this.props.shortUrl} disabled={this.props.isDisabled} />
-        <h1>{this.props.longUrl}</h1>
+        <input
+          className="url-output-field"
+          type="text"
+          value={this.props.shortUrl}
+          disabled={this.props.isDisabled}
+          onFocus={(event) => this.handleFocus(event)} />
       </div>
     )
   }
