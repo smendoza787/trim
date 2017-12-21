27a18102fdb75af507e9846afda8f8fbba408256
Sergio Mendoza <smendoza787@gmail.com>
2017-12-20 23:56:27 -0800

    add copy label on hover, and copied label on copy

--- client/src/components/UrlOutput/index.js
+++ client/src/components/UrlOutput/index.js
@@ -2,28 +2,64 @@ import React, { Component } from 'react';
 import './index.css';
 
 class UrlOutput extends Component {
-  handleFocus(event) {
-    event.target.select();
-  }
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      isHovered: false,
+      isCopied: false
+    }
+
+    this.handleHover = this.handleHover.bind(this);
+    this.setCopyListener = this.setCopyListener.bind(this);
+  }
+  
   componentDidUpdate(prevProps, prevState) {
     if (this.props.linkSelected) {
       let obj = this.refs.urlOutput;
       obj.select();
+      this.setCopyListener(this.props.shortUrl);
     }
   }
+
+  setCopyListener(url) {
+    document.addEventListener('copy', (e) => {
+      this.setState({ isCopied: true });
+    });
+  }
   
+  handleFocus(event) {
+    event.target.select();
+  }
+
+  handleHover() {
+    if (this.props.linkSelected) {
+      this.setState({ isHovered: !this.state.isHovered });
+    }
+  }
+
   render() {
+    const labelClass = this.state.isHovered && !this.state.isCopied ? "copy-label" : "hidden";
+    const copiedClass = this.state.isCopied ? "copied-label" : "hidden";
     return (
-      <div className="url-output">
-        <h3>Shortened URL</h3>
-        <input
-          ref="urlOutput"
-          className="url-output-field"
-          type="text"
-          value={this.props.shortUrl}
-          disabled={this.props.isDisabled}
-          onFocus={(event) => this.handleFocus(event)} />
+      <div 
+        className="url-output"
+        onMouseEnter={this.handleHover}
+        onMouseLeave={this.handleHover}>
+          <h3>Shortened URL</h3>
+          <input
+            ref="urlOutput"
+            className="url-output-field"
+            type="text"
+            value={this.props.shortUrl}
+            disabled={this.props.isDisabled}
+            onFocus={(event) => this.handleFocus(event)} />
+          <div className={labelClass}>
+            Press &#8984; + C to copy
+          </div>
+          <div className={copiedClass}>
+            Copied!
+          </div>
       </div>
     )
   }
