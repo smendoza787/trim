b5c35ee45549be91a8fd3f92e02f945fb13ce36d
Sergio Mendoza <smendoza787@gmail.com>
2017-12-21 00:49:41 -0800

    import contact icons and center them

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import phone from '../../images/phone.png';
+import email from '../../images/email.png';
 import './index.css';
 
 const Footer = () =>
   <div className="footer">
     <div className="contact">
+      <div className="contact-phone">
+        <img src={phone} alt="phone" height="20" />
         <a href="/">+14248293749</a>
+      </div>
+      <div className="contact-email">
+        <img src={email} alt="email" height="20"/>
         <a href="/">thisisatest@gmail.com</a>
+      </div>
     </div>
     <div className="copyright">
       &copy; The Testing Company. All Rights Reserved.
